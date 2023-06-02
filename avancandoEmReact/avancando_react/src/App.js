8ea2f6dafc350281c763f1c97ed463699b405263
Jenifer Kindermann <jeniferkindermann@gmail.com>
2023-06-02 19:22:43 -0300

    Desafio 4

--- avancandoEmReact/avancando_react/src/App.js
+++ avancandoEmReact/avancando_react/src/App.js
@@ -1,24 +1,90 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
-import img2 from "./assets/img2.png"
-import ManageData from './components/ManageData';
-import ListRender from './components/ListRender';
 import CondicionalRender from './CondicionalRender';
+import img2 from "./assets/img2.png";
+import CarDetails from './components/CarDetails';
+import ChangeMessageState from './components/ChangeMessageState';
+import Container from './components/Container';
+import ExecuteFunction from './components/ExecuteFunction';
+import Fragment from './components/Fragment';
+import ListRender from './components/ListRender';
+import ManageData from './components/ManageData';
+import Message from './components/Message';
+import ShowUserName from './components/ShowUserName';
 
 function App() {
+
+  const [userName] = useState("Maria")
+
+  const cars = [
+    { id: 1, brand: "Ferrari", color: "Amarelo", newCar: true, km: 0 },
+    { id: 1, brand: "KIA", color: "Branco", newCar: false, km: 44440 },
+    { id: 1, brand: "Renault", color: "Azul", newCar: false, km: 56890 },
+  ]
+
+  function showMessage() {
+    console.log("Evento do componente pai")
+  }
+
+  const [message, setMessage] = useState("")
+  const handleMessage = (msg) => {
+    setMessage(msg)
+  }
+
+  
+
+
+
   return (
     <div className="App">
       <h1>Avançando em React</h1>
       {/*Imagem em public */}
       <div>
-        <img src = "/img1.png" alt="foto minha"/>
+        <img src="/img1.png" alt="foto minha" />
       </div>
       <div>
-        <img src ={img2} alt = "foto"/>
+        <img src={img2} alt="foto" />
       </div>
-      <ManageData/>
-      <ListRender/>
-      <CondicionalRender/>
+      <ManageData />
+      <ListRender />
+      <CondicionalRender />
+      {/*props*/}
+      <ShowUserName name={userName} />
+      {/*destructuring*/}
+      <CarDetails brand="VW" km={1000000} Color="Azul" newCar={false} />
+      {/*reaproveitando*/}
+      <CarDetails brand="ford" km={0} Color="Azul" newCar={true} />
+      <CarDetails brand="fiat" km={554544} Color="Azul" newCar={false} />
+
+      {/*Loop em array de objetos*/}
+      {cars.map((car) => (
+        <CarDetails
+          key={car.id}
+          brand={car.brand}
+          color={car.color}
+          km={car.km}
+          newCar={car.newCar}
+        />
+      ))}
+
+      <Fragment propFragment="teste" />
+      {/*children*/}
+      <Container>
+        <p>este é o conteudo</p>
+      </Container>
+
+
+      {/*executar função*/}
+      <ExecuteFunction myFunction={showMessage} />
+
+      {/*state lift*/}
+      <Message msg={message} />
+      <ChangeMessageState handleMessage={handleMessage} />
+
+      {/*Desafio 4*/}
+      
+
+
     </div>
   );
 }
