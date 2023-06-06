492b85e9b6945547872e977e027a29cfec0f8be7
Jenifer Kindermann <jeniferkindermann@gmail.com>
2023-06-06 11:02:56 -0300

    Feito desafio de css com globalestilização de escopo, module.css

---
+++ CSS_REACT/challengecss/src/components/CarDetails.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import styles from './CarDetails.module.css'
+
+const CarDetails = ({car}) => {
+    
+  return (
+    <div className={styles.card}>
+        <h1>{car.name}</h1>
+        <p>KM: {car.km} </p>
+        <p>Cor: {car.color}</p>
+
+    </div>
+  )
+}
+
+export default CarDetails
